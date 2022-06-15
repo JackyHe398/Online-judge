5c1a669b54ad3aa0841619d90a749fcdb08a915a
JackyHe398 <62739796+JackyHe398@users.noreply.github.com>
2022-06-15 16:04:30 +0800

    Update min_3.py

--- min_3.py
+++ min_3.py
@@ -6,12 +6,9 @@ def getMin3(data):
   min.sort()
   for i in data[3:]:
     if min[0] > i:
-      min[2] = min[1]
-      min[1] = min[0]
-      min[0] = i
+      min[2], min[1], min[0] = min[1], min[0], i
     elif min[1] > i:
-      min[2] = min[1]
-      min[1] = i
+      min[2], min[1] = min[1], i
     elif min[2] > i:
       min[2] = i
   print(min[2])
