34b422568aff0ad2b0c2325f5f8e0f7b1b3c9a66
JackyHe398 <62739796+JackyHe398@users.noreply.github.com>
2022-06-15 15:15:26 +0800

    Create min_3.py

---
+++ min_3.py
@@ -0,0 +1,17 @@
+def getMin3(data):
+  if len(data) < 3:
+    print("NA")
+    return
+  min = data[:3]
+  min.sort()
+  for i in data[3:]:
+    if min[0] > i:
+      min[2] = min[1]
+      min[1] = min[0]
+      min[0] = i
+    elif min[1] > i:
+      min[2] = min[1]
+      min[1] = i
+    elif min[2] > i:
+      min[2] = i
+  print(min[2])
